89d294f8ae4c1b3f4e910433f9277aaf6f31de8d
Estefania <estefaniacastellanos802@gmail.com>
2025-11-05 19:31:49 +0100

    BASEGRAPHQL

---
+++ api-graphql/src/test/java/proyecto/api_graphql/ApiGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.api_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
